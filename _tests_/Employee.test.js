9f3b44bd26adf0e08af94b488f4838535be161be
Jessica Kyung In Lee <99236183+kyunginlee@users.noreply.github.com>
2022-04-27 16:14:44 +1000

    built test unit for employee

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,28 @@
+const Employee = require("../lib/Employee.js");
+
+describe("ProfileInfo", () => {
+	it("should return the employee details when requested)
+	const name = "Paddington Bear";
+	const empId = "111";
+	const email = "paddington@bond.com";
+	const officeNo = "1";
+	
+	const employeeTest = new Employee(name, empId, email. officeNo);
+	it("should return employee's name", () => {
+		expect(employeeTest.getName()).toBe(name);
+	});
+	
+	it("should return employee's employee ID", () => {
+		expect(employeeTest.getId()).toBe(empId);
+	});
+
+	it("should return employee's email", () => {
+		expect(employeeTest.getEmail()).toBe(email);
+	});	
+
+		it("should return employee's office number", () => {
+		expect(employeeTest.getOffice()).toBe(officeNo);
+	});
+});
+
+
