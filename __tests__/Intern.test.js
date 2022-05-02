d16f2ee56397ffe83e9af6eac8025091c9ed3114
KyungIn Lee <jess.lee2906@gmail.com>
2022-05-02 23:02:47 +1000

    Both Intern and Manager unit tests and Classes updated and passed

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,38 +1,25 @@
 const Intern = require("../lib/Intern.js");
 
-describe("Intern", () => {
-	it("should return the interns details when requested")
-	const name = "Ted";
-	const id = "333";
-	const email = "ted@wahlberg.com";
-	const officeNo = "3";
+
+	// const name = "Ted";
+	// const id = "333";
+	// const email = "ted@wahlberg.com";
+	// const officeNo = "3";
+	// const position = "Intern";
+	// const school = "school";
+	
+	
+it("should return intern's position", () => {
 	const position = "Intern";
+	const internTest = new Intern(" ", 1, position);
+	expect(internTest.position).toBe(position);
+});
+
+
+it("should return school intern attended", () => {
 	const school = "school";
-	
-	const internTest = new Intern(name, id, email, officeNo, position);
-	it("should return intern's name", () => {
-		expect(internTest.getName()).toBe(name);
-	});
-	
-	it("should return intern's employee ID", () => {
-		expect(internTest.getId()).toBe(id);
-	});
-
-	it("should return intern's email", () => {
-		expect(internTest.getEmail()).toBe(email);
-	});	
-
-	it("should return intern's office number", () => {
-		expect(internTest.getOfficeNo()).toBe(officeNo);
-	});
-	
-	it("should return intern's position", () => {
-		expect(internTest.getPosition()).toBe(position);
-	});
-
-	it("should return school intern attended", () => {
-		expect(internTest.getSchool()).toBe(school);
-	});
+	const internTest = new Intern(" ", 1, " ", 1, school);
+	expect(internTest.school).toBe(school);
 });
 
 
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,33 +1,15 @@
 const Manager = require("../lib/Manager.js");
 
-describe("Manager", () => {
-	it("should return the managers details when requested")
-	const name = "Yogi Bear";
-	const id = "444";
-	const email = "yogi@boo-boo.com";
-	const officeNo = "4";
+
+	// const name = "Yogi Bear";
+	// const id = "444";
+	// const email = "yogi@boo-boo.com";
+	// const officeNo = "4";
+	// const position = "Manager";
+	
+	
+it("should return manager's position", () => {
 	const position = "Manager";
-	
-	const managerTest = new Manager(name, id, email, officeNo, position);
-	it("should return manager's name", () => {
-		expect(managerTest.getName()).toBe(name);
-	});
-	
-	it("should return manager's employee ID", () => {
-		expect(managerTest.getId()).toBe(id);
-	});
-
-	it("should return manager's email", () => {
-		expect(managerTest.getEmail()).toBe(email);
-	});	
-
-	it("should return manager's office number", () => {
-		expect(managerTest.getOffice()).toBe(officeNo);
-	});
-	
-	it("should return manager's position", () => {
-		expect(managerTest.getPosition()).toBe(position);
-	});
+	const managerTest = new Manager(" ", 1, position);
+	expect(managerTest.position).toBe(position);
 });
-
-
